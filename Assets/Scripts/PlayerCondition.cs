89e3089f45312195d94ecf27e6d7182295b535de
hhd14725 <127010518+hhd14725@users.noreply.github.com>
2025-05-22 10:06:18 +0900

    feat: 플레이어 상태 UI base 구현

---
+++ Assets/Scripts/PlayerCondition.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public UICondition uiCondition;
+    Condition Health { get { return uiCondition.health; } }
+    Condition Hunger { get { return uiCondition.hunger; } }
+    Condition Stamina { get { return uiCondition.stamina; } }
+
+    public float noHungerHealthDecay;
+
+    private void Update()
+    {
+        Hunger.Subtract(Hunger.passiveValue * Time.deltaTime);
+        Stamina.Add(Stamina.passiveValue * Time.deltaTime);
+
+        if(Hunger.curValue == 0f)
+        {
+            Health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        if(Health.curValue == 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        Health.Add(amount);
+    }
+
+    public void Eat(float amount)
+    {
+        Hunger.Add(amount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("Player has died");
+     
+    }
+}
