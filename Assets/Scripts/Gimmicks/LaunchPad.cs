a1533b9d05247fa4c902c440d4ca4f3f888ba595
hhd14725 <127010518+hhd14725@users.noreply.github.com>
2025-05-23 01:55:12 +0900

    feat: 기본적인 점프 버섯 추가

---
+++ Assets/Scripts/Gimmicks/LaunchPad.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchPad : MonoBehaviour
+{
+    [Header("LaunchPad Settings")]
+    public float launchForce = 10f;
+    Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = CharacterManager.Instance.Player.GetComponent<Rigidbody>();
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.CompareTag("Player"))
+        {
+            if(_rigidbody != null)
+            {
+              
+                _rigidbody.AddForce(Vector3.up * launchForce, ForceMode.Impulse);
+            }
+        }
+    }
+ 
+
+}
