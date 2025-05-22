7992eeb373c51cc56888b55a1668888c8f223ba9
hhd14725 <127010518+hhd14725@users.noreply.github.com>
2025-05-23 01:11:29 +0900

    feat: 곰 인카운트할때 전투볼륨 구현

---
+++ Assets/Scripts/Player/FootSteps.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    public AudioClip[] footStepClips;
+    private AudioSource audioSource;
+    private Rigidbody _rigidbody;
+    public float footStepThreshold;
+    public float footStepRate;
+    private float footStepTime;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(Mathf.Abs(_rigidbody.velocity.y) < 0.1f)
+        {
+            if(_rigidbody.velocity.magnitude > footStepThreshold)
+            {
+                if(Time.time - footStepTime > footStepRate)
+                {
+                    footStepTime = Time.time;
+                    audioSource.PlayOneShot(footStepClips[Random.Range(0, footStepClips.Length)]);
+
+                }
+            }
+        }
+    }
+}
